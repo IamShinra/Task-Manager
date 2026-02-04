6585c53def4441c03fec885769b5ec9a3dd4ce7c
Shinra Zen <pratham18koli@gmail.com>
2026-02-04 12:49:09 +0530

    updating the api file

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "/api",
-    // baseURL: "http://localhost:5001/api",
-        baseURL: "https://task-manager-5y85.onrender.com/api",
+    baseURL: "http://localhost:5001/api",
+        // baseURL: "https://task-manager-5y85.onrender.com/api",
 });
 export default api;
